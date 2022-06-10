f5f9185fecc8d238e1fbad720e6039621e3ee353
Rutunj <rutunjp206@gmail.com>
2022-06-10 10:41:54 +0530

    added ALLOWED_HOSTS

--- adoption/settings.py
+++ adoption/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT= os.path.join(BASE_DIR,'static')
-
+ALLOWED_HOSTS = ['*']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
